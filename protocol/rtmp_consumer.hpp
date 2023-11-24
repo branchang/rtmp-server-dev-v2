3f71d40a9860ac2973ecfd23a05cbeb4097096dd
ran.zhang <ran.zhang@vhall.com>
2023-11-24 11:16:23 +0800

    抽离consumer

---
+++ protocol/rtmp_consumer.hpp
@@ -0,0 +1,52 @@
+#ifndef RS_RTMP_CONSUMER_HPP
+#define RS_RTMP_CONSUMER_HPP
+
+#include <common/core.hpp>
+#include <common/connection.hpp>
+#include <protocol/rtmp_jitter.hpp>
+
+namespace rtmp
+{
+
+class MessageQueue;
+class Source;
+
+class IWakeable
+{
+public:
+    IWakeable();
+    virtual ~IWakeable();
+
+public:
+    virtual void WakeUp() = 0;
+};
+
+class Consumer : public IWakeable
+{
+public:
+    Consumer(Source *s, Connection *c);
+    virtual ~Consumer();
+
+public:
+    virtual void SetQueueSize(double queue_size);
+    virtual int GetTime();
+    virtual int Enqueue(SharedPtrMessage *shared_msg, bool atc, JitterAlgorithm ag);
+    virtual int DumpPackets(MessageArray *msg_arr, int &count);
+    virtual void Wait(int nb_msgs, int duration);
+    virtual int OnPlayClientPause(bool is_pause);
+    //IWakeable
+    virtual void WakeUp() override;
+private:
+    Source *source_;
+    Connection *conn_;
+    bool pause_;
+    Jitter *jitter_;
+    MessageQueue *queue_;
+    st_cond_t mw_wait_;
+    bool mw_waiting_;
+    int mw_min_msgs_;
+    int mw_duration_;
+};
+
+}
+#endif
--- protocol/rtmp_source.hpp
+++ protocol/rtmp_source.hpp
@@ -5,6 +5,7 @@
 #include <common/queue.hpp>
 #include <protocol/rtmp_stack.hpp>
 #include <protocol/rtmp_jitter.hpp>
+#include <protocol/rtmp_consumer.hpp>
 
 class Dvr;
 
@@ -22,43 +23,6 @@ public:
 public:
     virtual int OnPublish(Source *s, Request *r) = 0;
     virtual int OnUnPublish(Source *s, Request *r) = 0;
-};
-
-class IWakeable
-{
-public:
-    IWakeable();
-    virtual ~IWakeable();
-
-public:
-    virtual void WakeUp() = 0;
-};
-
-class Consumer : public IWakeable
-{
-public:
-    Consumer(Source *s, Connection *c);
-    virtual ~Consumer();
-
-public:
-    virtual void SetQueueSize(double queue_size);
-    virtual int GetTime();
-    virtual int Enqueue(SharedPtrMessage *shared_msg, bool atc, JitterAlgorithm ag);
-    virtual int DumpPackets(MessageArray *msg_arr, int &count);
-    virtual void Wait(int nb_msgs, int duration);
-    virtual int OnPlayClientPause(bool is_pause);
-    //IWakeable
-    virtual void WakeUp() override;
-private:
-    Source *source_;
-    Connection *conn_;
-    bool pause_;
-    Jitter *jitter_;
-    MessageQueue *queue_;
-    st_cond_t mw_wait_;
-    bool mw_waiting_;
-    int mw_min_msgs_;
-    int mw_duration_;
 };
 
 class MessageQueue
